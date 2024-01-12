4ccb455abb99910bdcbae0ee384b1444767920de
gzbosshard <gzbosshard@gmail.com>
2024-01-12 09:21:41 -0300

    implementação de herança

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123", "modelo", "imei", 5);
+        nokia.InstalarAplicativo("appNokia");
+
+        Smartphone iphone = new Iphone("123", "modelo", "imei", 5);
+        iphone.InstalarAplicativo("appIphone");
+
+        Console.ReadKey();
+
+    }
+}
